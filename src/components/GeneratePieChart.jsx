e89bd227f18eda47e1fdc25a9224dd90f1d6348a
ashayk <ashayk@cybage.com>
2022-03-16 18:08:21 +0530

    Changes for pie chart implementation using Canvasjs.

---
+++ src/components/GeneratePieChart.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import CanvasJSReact from "./canvasjs.react";
+
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
+const GeneratePieChart = (props) => {
+  const { options } = props;
+  console.log(options);
+  return (
+    <div>
+      <CanvasJSChart
+        options={options}
+        /* onRef={ref => this.chart = ref} */
+      />
+      {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+    </div>
+  );
+};
+
+export default GeneratePieChart;
--- src/pages/DisplayResult/index.jsx
+++ src/pages/DisplayResult/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import questionsArray from "../../questions";
 import { useSelector } from "react-redux";
+import GeneratePieChart from "../../components/GeneratePieChart";
+
 const DisplayResult = () => {
   const usersArray = useSelector((state) => state.userArray);
   const correctAnswersArray = questionsArray.filter((element, index) => {
@@ -10,10 +12,41 @@ const DisplayResult = () => {
     }
     return correctAnswer;
   });
+  const correctPercentage =
+    (correctAnswersArray.length / questionsArray.length) * 100;
+  const wrongPercentage = 100 - correctPercentage;
 
-  console.log(correctAnswersArray.length);
+  const chartClickEvent = (e) => {
+    console.log(e);
+  };
+  const options = {
+    exportEnabled: true,
+    animationEnabled: true,
+    title: { text: "Result" },
 
-  return <h1>Answers Array</h1>;
+    data: [
+      {
+        type: "pie",
+        startAngle: 90,
+        toolTipContent: "<b>{label}</b>: {y}%",
+        showInLegend: "true",
+        legendText: "{label}",
+        indexLabelFontSize: 16,
+        indexLabel: "{label} - {y}%",
+        dataPoints: [
+          {
+            y: correctPercentage,
+            label: "Correct",
+            onclick,
+            click: chartClickEvent,
+          },
+          { y: wrongPercentage, label: "Wrong", click: chartClickEvent },
+        ],
+      },
+    ],
+  };
+
+  return <GeneratePieChart options={options} />;
 };
 
 export default DisplayResult;

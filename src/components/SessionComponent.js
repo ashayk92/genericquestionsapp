39523640f205dd7353068f2d4625ab6cd1961f64
ashayk <ashayk@cybage.com>
2022-03-17 12:40:32 +0530

    Removed unwanted console.

--- src/components/SessionComponent.js
+++ src/components/SessionComponent.js
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router";
 const SessionComponent = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  console.log("User logged in");
 
   setTimeout(() => {
     navigate("/");
